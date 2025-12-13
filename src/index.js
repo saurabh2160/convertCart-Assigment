3cc509ca4c2f8d2bec2415305ab1a3851242c4c6
saurabh2160 <saurabhmanohar90@gmail.com>
2025-12-13 14:32:38 +0530

    cors policy revoked

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN); 
+  res.setHeader('Access-Control-Allow-Origin', "*"); 
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   next();
 });
